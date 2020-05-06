2756f21094e93bd18d41a71c33bc29fabf30bb73
tanviredu <ornobtanvir.git@gmail.com>
2020-05-06 15:07:36 +0600

    added

--- mywork/ConsoleApp1/ConsoleApp1/Program.cs
+++ mywork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ArdalisRating
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ardalis Insurance Rating System Starting...");
+
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating produced.");
+            }
+
         }
     }
 }
